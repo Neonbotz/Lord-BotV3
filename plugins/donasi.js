e272406cd23d272a7208499f883d5859cf52419f
Neon mD <106319373+Neonbotz@users.noreply.github.com>
2022-06-10 09:53:56 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,14 +1,14 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `››╭─〘 *Donasi* 〙
 ╭╡
-│┝‷✧ *Pulsa:* 6287708773367
-│┝‷✧ *Dana:* 6285712420674
-│┝‷✧ *Linkaja:* 6285712420674
-│┝‷✧ *Gopay:* 6285712420674
-│┝‷✧ *Ovo:* 6285712420674
+│┝‷✧ *Pulsa:* 6287719424487
+│┝‷✧ *Dana:* 
+│┝‷✧ *Linkaja:* 
+│┝‷✧ *Gopay:* 
+│┝‷✧ *Ovo:* 
 │╰───···─────
 │⁺◛˖ Ingin Donasi? Chat
-│wa.me/6288229683561?text=kak+mau+donasi
+│wa.me/6287719424487?text=kak+mau+donasi
 ╰──────────···───╮
 ▌│█║▌║▌║║▌║▌║█│▌
 `.trim(), `${wm}`, 'Owner', '.owner')
